debd0b8856d13af0e5f59927b0bca8f09d8920db
hanifazhar <azharhanif04@gmail.com>
2025-02-25 15:23:16 +0700

    add update pasien API

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const conn = require('./config/db.js');
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("Server berjalan di port 3000")
 });
@@ -29,7 +31,7 @@ app.get('/get-data-pasien', function(req, res) {
 app.post('/add-pasien', function (req, res) {
     const { nama, alamat, jenis_kelamin, berat_badan, tinggi_badan, golongan_darah, usia, tensi, no_hp } = req.body;
 
-    const queryStr = " INSERT INTO pasien ( nama, alamat, jenis_kelamin, berat_badan, tinggi_badan, golongan_darah, usia, tensi, no_hp VALUES (?,?,?,?,?,?,?,?,?)) "
+    const queryStr = ` INSERT INTO pasien ( nama, alamat, jenis_kelamin, berat_badan, tinggi_badan, golongan_darah, usia, tensi, no_hp ) VALUES (?,?,?,?,?,?,?,?,?) `
     conn.query(queryStr, [nama, alamat, jenis_kelamin, berat_badan, tinggi_badan, golongan_darah, usia, tensi, no_hp], function (err, results) {
         if (err) {
             console.log(err);
@@ -44,5 +46,49 @@ app.post('/add-pasien', function (req, res) {
             "message": "berhasil menambahkan data pasien",
             "data": results
         });
+    })
+})
+
+app.get('/get-data-pasien-by-id', function(req, res) {
+    const pasienId = req.query.id;
+    const queryStr = `SELECT * FROM pasien WHERE id = ?`;
+    conn.query(queryStr, [pasienId], function(err, results) {
+       if (err) {
+        return res.status(500).json({ message: 'Gagal Menampilkan Data Pasien' });
+       }
+       if (results.length === 0) {
+        return res.status(404).json({ message: 'Pasien tidak ditemukan' });
+       }
+       res.status(200).json ({
+           "success": true,
+           "message": "Berhasil menampilkan data pasien",
+           "data": results
+       });
+   })
+})
+
+app.post('/update-pasien', function (req, res) {
+    const { nama, alamat, jenis_kelamin, berat_badan, tinggi_badan, golongan_darah, usia, tensi, no_hp } = req.body;
+    const pasienId = req.query.id;
+
+    if(!nama || !alamat || !jenis_kelamin || !berat_badan || !tinggi_badan || !golongan_darah || !usia || !tensi || !no_hp) {
+        return res.status(400).json({ message: "Data Tidak Boleh Kosong"});
+    }
+
+    const queryStr = `SELECT nama, alamat, jenis_kelamin, berat_badan, tinggi_badan, golongan_darah, usia, tensi, no_hp FROM pasien WHERE id = ?`;
+    conn.query(queryStr, [nama, alamat, jenis_kelamin, berat_badan, tinggi_badan, golongan_darah, usia, tensi, no_hp], function (err, results) {
+        if (err) {
+            console.log(err)
+            return res.status(500).json ({
+                "success": false,
+                "message": "gagal mengubah data pasien",
+                "data": err.message
+            })
+        }
+        return res.status(200).json({
+            "success": true,
+            "message": "berhasil mengubah data pasien",
+            "data": results
+        })
     })
 })
