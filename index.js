f06466426337730f5df373b07e5ec1a633574bc8
hanifazhar <azharhanif04@gmail.com>
2025-02-25 14:22:20 +0700

    menambahkan data pasien API

--- index.js
+++ index.js
@@ -25,3 +25,24 @@ app.get('/get-data-pasien', function(req, res) {
         });
     })
 })
+
+app.post('/add-pasien', function (req, res) {
+    const { nama, alamat, jenis_kelamin, berat_badan, tinggi_badan, golongan_darah, usia, tensi, no_hp } = req.body;
+
+    const queryStr = " INSERT INTO pasien ( nama, alamat, jenis_kelamin, berat_badan, tinggi_badan, golongan_darah, usia, tensi, no_hp VALUES (?,?,?,?,?,?,?,?,?)) "
+    conn.query(queryStr, [nama, alamat, jenis_kelamin, berat_badan, tinggi_badan, golongan_darah, usia, tensi, no_hp], function (err, results) {
+        if (err) {
+            console.log(err);
+            return res.status(500).json ({
+                "success": false,
+                "message": "gagal menyimpan data pasien",
+                "data": err.message
+            })
+        }
+        res.status(200).json ({
+            "success": true,
+            "message": "berhasil menambahkan data pasien",
+            "data": results
+        });
+    })
+})
