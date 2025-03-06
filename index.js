6cd3a13af97f25f6e1b7d6969d54a0c8ea5a8362
hanifazhar <azharhanif04@gmail.com>
2025-03-06 15:35:32 +0700

    fix error

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use("/api", router);
+app.use(router);
 
 app.listen(PORT, () => {
     console.log("Server berjalan di port 3000")
--- routes/pasienRoute.js
+++ routes/pasienRoute.js
@@ -11,8 +11,8 @@ const router = express.Router();
 
 router.get('/get-data-pasien', getDataPasien);
 router.post('/add-pasien', addPasien);
-router.get('/get-data-pasien/:id', getDataPasienById);
-router.post('update-pasien', updatePasien);
-router.post('delete-pasien',deletePasien);
+router.get('/get-data-pasien-by-id', getDataPasienById);
+router.post('/update-pasien', updatePasien);
+router.post('/delete-pasien',deletePasien);
 
 export default router;
